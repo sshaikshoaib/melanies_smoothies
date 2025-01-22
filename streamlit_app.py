0c18e3a94d9e5b1963b9ec731896cdbe34cd3702
sshaikshoaib <145051080+sshaikshoaib@users.noreply.github.com>
2025-01-22 23:23:53 +0530

    Update streamlit_app.py
    added json

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,4 +59,4 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
