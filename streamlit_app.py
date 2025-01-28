96d866fd7102a499374135731ad45f9b672dfc37
sshaikshoaib <145051080+sshaikshoaib@users.noreply.github.com>
2025-01-28 18:28:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,55 +9,35 @@ st.write(
     """
 )
 
-
-
 name_on_order = st.text_input("Name on Smoothie :")
-st.write("The name on your Smoothgie will be :", name_on_order)
+st.write("The name on your Smoothie will be :", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
+# Convert dataframe to list of fruits
+fruits = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
 
-
-ingredients_list =st.multiselect(
-    'Choose up to 5 ingredients:'
-    ,my_dataframe
-    ,max_selections = 5
-    )
-
-
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    fruits,  # Passing list of fruits
+    max_selections=5
+)
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
+    ingredients_string = ' '.join(ingredients_list)  # More Pythonic way to join the list
     
-    ingredients_string = ''
+    my_insert_stmt = """ 
+        insert into smoothies.public.orders(ingredients,name_on_order)
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen +' '
-    
-    #st.write(ingredients_string)
-
-
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
-
-    # st.write(my_insert_stmt)
-    # st.stop()
-
-
     time_to_insert = st.button('submit Order')
-
-
+    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered! '+name_on_order, icon="✅")
-
+        st.success(f'Your Smoothie is ordered! {name_on_order}', icon="✅")
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
-
